fc0556d46012c56b1556ea6abd96dc25dc06d538
Siwei Cui <jncsw@163.com>
2024-03-21 15:08:23 -0500

    ppl eval with lm_eval

---
+++ perplexity/ppl_eval.py
@@ -0,0 +1,255 @@
+
+# Ref: github.com/IST-DASLab/sparsegpt
+
+from transformers import AutoTokenizer, TextStreamer
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import math
+import time
+import random
+import torch
+import torch.nn as nn
+import transformers
+from datasets import load_dataset
+
+
+def get_loaders(name, nsamples=128, seed=0, seqlen=2048, model=''):
+    tokenizer = get_tokenizer(model)
+    if 'wikitext2' in name:
+        return get_wikitext2(nsamples, seed, seqlen, model, tokenizer)
+    if 'ptb' in name:
+        return get_ptb(nsamples, seed, seqlen, model, tokenizer)
+    if 'c4' in name:
+        return get_c4(nsamples, seed, seqlen, model, tokenizer)
+
+
+    
+def set_seed(seed):
+    np.random.seed(seed)
+    torch.random.manual_seed(seed)
+
+def get_tokenizer(model):
+    if "llama" in model.lower():
+        tokenizer = LlamaTokenizer.from_pretrained(model, use_fast=False, cache_dir="/scratch/user/siweicui/LLM/huggingface/")
+        # fix for transformer 4.28.0.dev0 compatibility
+        if tokenizer.bos_token_id != 1 or tokenizer.eos_token_id != 2:
+            try:
+                tokenizer.bos_token_id = 1
+                tokenizer.eos_token_id = 2
+            except AttributeError:
+                pass
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(model, use_fast=False, cache_dir="/scratch/user/siweicui/LLM/huggingface/")
+    return tokenizer
+
+def get_wikitext2(nsamples, seed, seqlen, model, tokenizer):
+    
+    traindata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='train', cache_dir="/scratch/user/siweicui/LLM/huggingface/")
+    testdata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test', cache_dir="/scratch/user/siweicui/LLM/huggingface/")
+
+    trainenc = tokenizer(" ".join(traindata['text']), return_tensors='pt')
+    testenc = tokenizer("\n\n".join(testdata['text']), return_tensors='pt')
+
+    random.seed(seed)
+    trainloader = []
+    for _ in range(nsamples):
+        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+        j = i + seqlen
+        inp = trainenc.input_ids[:, i:j]
+        tar = inp.clone()
+        tar[:, :-1] = -100
+        trainloader.append((inp, tar))
+    return trainloader, testenc
+
+def get_ptb(nsamples, seed, seqlen, model, tokenizer):
+    traindata = load_dataset('ptb_text_only', 'penn_treebank', split='train', cache_dir="/scratch/user/siweicui/LLM/huggingface/")
+    testdata = load_dataset('ptb_text_only', 'penn_treebank', split='test', cache_dir="/scratch/user/siweicui/LLM/huggingface/")
+
+    trainenc = tokenizer(" ".join(traindata['sentence']), return_tensors='pt')
+    testenc = tokenizer(" ".join(testdata['sentence']), return_tensors='pt')
+
+    random.seed(seed)
+    trainloader = []
+    for _ in range(nsamples):
+        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+        j = i + seqlen
+        inp = trainenc.input_ids[:, i:j]
+        tar = inp.clone()
+        tar[:, :-1] = -100
+        trainloader.append((inp, tar))
+    return trainloader, testenc
+
+def get_c4(nsamples, seed, seqlen, model, tokenizer):
+    traindata = load_dataset(
+        'allenai/c4', 'allenai--c4', data_files={'train': 'en/c4-train.00000-of-01024.json.gz'}, split='train', cache_dir="/scratch/user/siweicui/LLM/huggingface/"
+    )
+    valdata = load_dataset(
+        'allenai/c4', 'allenai--c4', data_files={'validation': 'en/c4-validation.00000-of-00008.json.gz'}, split='validation', cache_dir="/scratch/user/siweicui/LLM/huggingface/"
+    )
+
+    random.seed(seed)
+    trainloader = []
+    for _ in range(nsamples):
+        while True:
+            i = random.randint(0, len(traindata) - 1)
+            trainenc = tokenizer(traindata[i]['text'], return_tensors='pt')
+            if trainenc.input_ids.shape[1] > seqlen:
+                break
+        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+        j = i + seqlen
+        inp = trainenc.input_ids[:, i:j]
+        tar = inp.clone()
+        tar[:, :-1] = -100
+        trainloader.append((inp, tar))
+
+    valenc = tokenizer(' '.join(valdata[:1100]['text']), return_tensors='pt')
+    valenc = valenc.input_ids[:, :(256 * seqlen)]
+
+    class TokenizerWrapper:
+        def __init__(self, input_ids):
+            self.input_ids = input_ids
+    valenc = TokenizerWrapper(valenc)
+
+    return trainloader, valenc
+
+# model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1" # OOM on 2XA100 (80GB Total).
+model_id = "mistralai/Mistral-7B-Instruct-v0.1"
+tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir="/scratch/user/siweicui/LLM/huggingface/")
+dataset = 'wikitext2' # Dataset: ['wikitext2', 'ptb', 'c4']
+device = "cuda" # the device to load the model onto
+
+model = AutoModelForCausalLM.from_pretrained(model_id,  cache_dir="/scratch/user/siweicui/LLM/huggingface/") # device_map="auto",
+# model.to(device)
+
+# messages = [
+#     {"role": "user", "content": "What is your favourite condiment?"},
+#     {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
+#     {"role": "user", "content": "Do you have mayonnaise recipes?"}
+# ]
+
+# encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
+
+# model_inputs = encodeds.to(device)
+# model.to(device)
+
+# generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+# decoded = tokenizer.batch_decode(generated_ids)
+# print(decoded[0])
+
+def find_layers(module, layers=[nn.Conv2d, nn.Linear], name=''):
+    if type(module) in layers:
+        return {name: module}
+    res = {}
+    for name1, child in module.named_children():
+        res.update(find_layers(
+            child, layers=layers, name=name + '.' + name1 if name != '' else name1
+        ))
+    return res
+
+model.eval()
+model.seqlen = 2048
+
+seed = 0
+
+
+
+dataloader, testloader = get_loaders(  
+        dataset, seed=seed, model=model_id, seqlen=model.seqlen
+)
+
+
+@torch.no_grad()
+def ppl_eval(model, testenc, dev, dataset: str, log_wandb: bool = False):
+    print("Evaluating ...")
+
+    testenc = testenc.input_ids
+    nsamples = testenc.numel() // model.seqlen
+
+    use_cache = model.config.use_cache
+    model.config.use_cache = False
+    layers = model.model.layers
+
+    model.model.embed_tokens = model.model.embed_tokens.to(dev)
+    layers[0] = layers[0].to(dev)
+
+    dtype = next(iter(model.parameters())).dtype
+    inps = torch.zeros(
+        (nsamples, model.seqlen, model.config.hidden_size), dtype=dtype, device=dev
+    )
+    cache = {"i": 0, "attention_mask": None}
+
+    class Catcher(nn.Module):
+        def __init__(self, module):
+            super().__init__()
+            self.module = module
+
+        def forward(self, inp, **kwargs):
+            inps[cache["i"]] = inp
+            cache["i"] += 1
+            cache["attention_mask"] = kwargs["attention_mask"]
+            raise ValueError
+
+    layers[0] = Catcher(layers[0])
+    for i in range(nsamples):
+        batch = testenc[:, (i * model.seqlen) : ((i + 1) * model.seqlen)].to(dev)
+        try:
+            model(batch)
+        except ValueError:
+            pass
+    layers[0] = layers[0].module
+
+    layers[0] = layers[0].cpu()
+    model.model.embed_tokens = model.model.embed_tokens.cpu()
+    torch.cuda.empty_cache()
+
+    outs = torch.zeros_like(inps)
+    attention_mask = cache["attention_mask"]
+
+    for i in range(len(layers)):
+        print(i)
+        layer = layers[i].to(dev)
+
+        # if args.gmp:
+        #     subset = find_layers(layer)
+        #     for name in subset:
+        #         W = subset[name].weight.data
+        #         thresh = torch.sort(torch.abs(W.flatten()))[0][
+        #             int(W.numel() * args.sparsity)
+        #         ]
+        #         W.data[torch.abs(W.data) <= thresh] = 0
+
+        for j in range(nsamples):
+            outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask)[0]
+        layers[i] = layer.cpu()
+        del layer
+        torch.cuda.empty_cache()
+        inps, outs = outs, inps
+
+    if model.model.norm is not None:
+        model.model.norm = model.model.norm.to(dev)
+    model.lm_head = model.lm_head.to(dev)
+
+    testenc = testenc.to(dev)
+    nlls = []
+    for i in range(nsamples):
+        hidden_states = inps[i].unsqueeze(0)
+        if model.model.norm is not None:
+            hidden_states = model.model.norm(hidden_states)
+        lm_logits = model.lm_head(hidden_states)
+        shift_logits = lm_logits[:, :-1, :].contiguous()
+        shift_labels = testenc[:, (i * model.seqlen) : ((i + 1) * model.seqlen)][:, 1:]
+        loss_fct = nn.CrossEntropyLoss()
+        loss = loss_fct(
+            shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)
+        )
+        neg_log_likelihood = loss.float() * model.seqlen
+        nlls.append(neg_log_likelihood)
+    ppl = torch.exp(torch.stack(nlls).sum() / (nsamples * model.seqlen))
+    print(f"Perplexity: {ppl.item():3f}")
+    # if log_wandb:
+    #     wandb.log({f"{dataset}/perplexity": ppl.item()})
+
+    model.config.use_cache = use_cache
+
+
+DEV = torch.device('cuda:0')
+ppl_eval(model, testloader, DEV, dataset)
